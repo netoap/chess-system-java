cd142de4ef5875ab6c807d8dd5c1d4d0a66780f0
netoap <alexanderneto5@gmail.com>
2018-08-03 14:39:45 +0100

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,4 +25,11 @@ public class Board {
 	public void setCols(int cols) {
 		this.cols = cols;
 	}
+	
+	public Piece piece(int row, int col) {
+		return pieces[row][col];
+	}
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
